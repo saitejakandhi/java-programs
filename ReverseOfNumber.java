777f9a8f68e6ef20af1e8d4d23db2507ccec7e0e
saitejakandhi <saitejakandhi29@gmail.com>
2025-12-16 19:41:37 +0530

    initial commit

--- ReverseOfNumber.java
+++ ReverseOfNumber.java
@@ -7,7 +7,7 @@ public class ReverseOfNumber {
         int num = sc.nextInt();
         System.out.println("Reverse of number");
         int reversed = 0;
-        while( num != 0){
+        while( num != 10){
             int digit = num % 10 ;
             reversed = reversed * 10 + digit;
             num /= 10;
